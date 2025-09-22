f596b444ec0f8967abe818609bd0474ac2834167
Frannnncccuzz <francescoiaselli@protonmail.com>
2025-09-22 23:31:13 +0200

    changing vite config again

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,15 @@
 // vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
       output: {
-        // mantieni hash per chunk/entry (JS/CSS)
-        entryFileNames: "assets/[name]-[hash].js",
-        chunkFileNames: "assets/[name]-[hash].js",
-        // niente hash per gli asset immagine
-        assetFileNames: ({ name }) => {
-          const extname = (name && name.split(".").pop()?.toLowerCase()) || "";
-          const isImage = ["jpg","jpeg","png","webp","gif","svg","avif"].includes(extname);
-          return isImage ? "assets/[name][extname]" : "assets/[name]-[hash][extname]";
-        },
-      },
-    },
-  },
-});
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
+})
